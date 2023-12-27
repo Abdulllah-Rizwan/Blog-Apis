0161e341c1c7f81cd806978df42e5adf35e466e4
Muhammad Abdullah <112887740+Abdulllah-Rizwan@users.noreply.github.com>
2023-12-27 12:51:49 +0000

    wrote utility functions

---
+++ src/utils/ApiError.js
@@ -0,0 +1,14 @@
+class ApiError extends Error {
+    constructor(statusCode,message="Something went wrong",errors=[],stack=""){
+        this.statusCode = statusCode;
+        this.message = message;
+        super(message);
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+
+        stack ? this.stack = stack : Error.captureStackTrace(this, this.constructor);
+
+
+    }
+}
